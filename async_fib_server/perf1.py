0ac40db9c153a2b6847cbaa226db7af6132f7e8c
framr <pavel.sht@yandex.ru>
2017-08-05 16:46:30 +0300

    script for testing performance

--- async_fib_server/perf1.py
+++ async_fib_server/perf1.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
+"""
+Measure response time for heavy request 
+"""
+
+import time
+
+from socket import socket, AF_INET, SOCK_STREAM
+
 
 
 if __name__ == "__main__":
 
-    pass
+    sock = socket(AF_INET, SOCK_STREAM)
+    sock.connect(('localhost', 25000))
+
+
+    while True:
+ 
+        start = time.time()       
+        sock.send(b'30')
+        resp = sock.recv(128)
+        if not resp:
+            break
+
+        print (time.time() - start)
+    
---
+++ async_fib_server/perf2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""
+Measure rps for light requests
+"""
+
+
+if __name__ == "__main__":
+    pass
