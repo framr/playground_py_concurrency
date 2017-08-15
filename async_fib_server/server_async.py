ec31a26997be313ba096360452dcceabfacbdaf1
framr <pavel.sht@yandex.ru>
2017-08-15 12:13:26 +0300

    first working version of async server

--- async_fib_server/server_async.py
+++ async_fib_server/server_async.py
@@ -3,26 +3,44 @@
 
 from socket import AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR, socket 
 #from threading import Thread
-#from concurrent.futures import ProcessPoolExecutor as Pool
+from concurrent.futures import ProcessPoolExecutor as Pool
 from collections import deque
+from select import select
 
 from fib import fib
-
-
-#pool = Pool(4)
-
+pool = Pool(4)
 
 
 tasks = deque()
+waiters_recv = {}
+waiters_send = {}
+
 def run(tasks):
-    while tasks:
+
+    while any([tasks, waiters_recv, waiters_send]):
+
+        # wait for i/o
+        if not tasks:
+            can_recv, can_send, _ = select(waiters_recv, waiters_send, [])
+            if can_recv:
+                tasks.extend([waiters_recv[x] for x in can_recv])
+            if can_send:
+                tasks.extend([waiters_send[x] for x in can_send])
+
+        # process tasks
         try:
-            t = tasks.popleft()
-            x = next(t)
+            task = tasks.popleft()
+            why, who = next(task)
+
+            if why == "recv":
+                waiters_recv[who.fileno()] = task
+            elif why == "send":
+                waiters_send[who.fileno()] = task
+            else:
+                raise ValueError("wrong task type, should be recv or send")
+
         except StopIteration:
             print("task done")
-
-
 
 
 def fib_handler(client):
@@ -43,22 +61,27 @@ def fib_handler(client):
     print("handler closed")
 
 
-
-
 def fib_server(address):
     sock = socket(AF_INET, SOCK_STREAM)
     sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     sock.bind(address)
     sock.listen(5)
-    
+        
     while True:
         yield 'recv', sock
         client, addr = sock.accept()
         print("connection", addr)
         #Thread(target=fib_handler, args=(client,)).start()
-        fib_handler(client)
+        
+        tasks.append(
+            fib_handler(client)
+        )
 
 
 if __name__ == '__main__':
 
-    fib_server(('', 25000))
+    tasks.append(fib_server(('', 25000)))
+    run(tasks)
+
+
+
